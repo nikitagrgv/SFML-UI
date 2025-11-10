34f62777bb43234b048176928d476cbe4841b9e8
Nikita Grigorev <pettyaelse@gmail.com>
2025-11-10 23:15:56 +0700

    w

--- SFMLUI/res/round_border_mask_fragment.glsl
+++ SFMLUI/res/round_border_mask_fragment.glsl
@@ -1,13 +1,14 @@
 ﻿uniform vec2 u_size;
 uniform vec4 u_radius;
 
-float sdRoundedBox(in vec2 p, in vec2 b, in vec4 r )
+float sdRoundedBox(in vec2 p, in vec2 b, in vec4 r)
 {
-	r.xy = (p.x>0.0)?r.xy : r.zw;
-	r.x  = (p.y>0.0)?r.x  : r.y;
-	vec2 q = abs(p)-b+r.x;
-	return min(max(q.x,q.y),0.0) + length(max(q,0.0)) - r.x;
+	r.xy = (p.x > 0.0) ? r.xy : r.zw;
+	r.x  = (p.y > 0.0) ? r.x  : r.y;
+	vec2 q = abs(p) - b + r.x;
+	return min(max(q.x, q.y), 0.0) + length(max(q, 0.0)) - r.x;
 }
+
 void main()
 {
 	float x = (gl_TexCoord[0].x - 0.5) * u_size.x;
